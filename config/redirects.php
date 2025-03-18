9fc1e7d39b1d8626266861e820f49aeff99d892c
Tim Kelty <tim@craftcms.com>
2025-03-18 10:06:00 -0400

    Boilerplate redirects

---
+++ config/redirects.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Redirection Rules
+ *
+ * [TODO: Short description about how they are triggered on 404s]
+ *
+ * Read all about Craft’s redirection behavior, here:
+ * https://craftcms.com/docs/5.x/system/routing.html#redirection
+ */
+
+return [
+
+];
