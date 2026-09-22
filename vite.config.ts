84f555dbaa219c325819b72bc2bfb9c20b67e5e0
Simon Wesp <s.wesp@gmx.net>
2026-09-22 13:51:13 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -50,7 +50,7 @@ export default defineConfig(({ command, mode }) => {
 		],
 		resolve: {
             alias: {
-              '@': path.resolve(__dirname, './src')
+              '@': path.resolve(import.meta.dirname, './src')
             },
             preserveSymlinks: true,
         },
